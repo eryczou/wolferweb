e53b1807c9021a2dd7846193f9d96ea466ad3dd3
Chen Fang <chenfanggm@hotmail.com>
2016-02-17 22:48:08 -0800

    [Home] home page text update

--- client/components/Blog/Blog.js
+++ client/components/Blog/Blog.js
@@ -8,35 +8,35 @@ class Blog extends React.Component {
         <div className='$color-text-primary'>
           <h1 className={classes.mainTitle}>WolferX</h1>
           <p className={classes.content}>
-            WolferX is <strong>end-to-end</strong> web application solution.<br/>
+            {`WolferX is end-to-end web application solution.`}
           </p>
           <h3 className={classes.subTitle}>
             Architecture
           </h3>
           <p className={classes.content}>
-            Nginx, Reactjs, Nodejs, Tomcat, Jersey, Cassandra, and MongoDB.
+            {`Nginx, Reactjs, Nodejs, MySQL`}
           </p>
           <h3 className={classes.subTitle}>
             Philosophy
           </h3>
           <p className={classes.content}>
-            Deploy the <strong>best practice</strong> of engineering
-            to every issues that will be encountered during web app implementation.
+            {`Deploy the best practice of engineering to issues
+            that encountered during implementation.`}
           </p>
           <h3 className={classes.subTitle}>
             Atmosphere
           </h3>
           <p className={classes.content}>
-            Here is for meet up! for group up! for talk and learn, for
-            <strong>not being alone when engineering something up</strong>.
+            {`Here is for meet up! group up! talk and learn!
+            for not being alone when gear something up.`}
           </p>
           <h3 className={classes.subTitle}>
             Goal
           </h3>
           <p className={classes.content}>
-            To build a solid web application end-to-end solution, that
-            helps any beautiful idea become a <strong>tangible product</strong>
-            easier, faster, and more reliable.
+            {`To build a solid web application end-to-end solution, that
+            helps any great idea become a tangible product easier,
+            faster, and reliable.`}
           </p>
         </div>
       </div>

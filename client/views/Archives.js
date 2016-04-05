64c48aa303c637c03a7d177e463702fb266e989b
Eyrc Zou <eryczou@gmail.com>
2016-04-04 21:18:38 -0400

    add eryc's test files

---
+++ client/views/Archives.js
@@ -0,0 +1,17 @@
+/**
+ * Created by zz232323 on 4/4/16.
+ */
+import React from 'react';
+
+export default class Archives extends React.Component {
+  render(){
+    return(
+      <div>
+        <h1>This is Eryc's Test file</h1>
+      </div>
+    )
+  }
+}
+
+
+

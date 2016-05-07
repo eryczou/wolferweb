7aa7d7f8046c5015cae0660959fc3002e7f00bd7
Chen Fang <chenfanggm@hotmail.com>
2016-05-07 00:07:18 -0700

    [rename] todolist to todoLIst

--- client/routes/TodoListView/containers/TodoList.js
+++ client/routes/TodoListView/containers/TodoList.js
@@ -1,6 +1,12 @@
 import React from 'react';
+<<<<<<< HEAD:client/routes/TodoListView/containers/TodoList.js
 import { addTodo } from '../modules/todoList'
+=======
+import { actions as TodoActions} from '../../redux/modules/todolist'
+>>>>>>> wolferian/master:client/containers/TodoList/Todo.js
 import { connect } from 'react-redux'
+import {bindActionCreators} from 'redux'
+import classes from './Todo.scss'
 
 class Todo extends React.Component {
   constructor() {
@@ -16,7 +22,15 @@ class Todo extends React.Component {
 
   handleSubmit(e) {
     e.preventDefault();
-    this.props.dispatch(addTodo(this.state.text));
+    this.props.actions.addTodo(this.state.text);
+  }
+
+  handleTodoComplete(id) {
+    this.props.actions.toggleTodo(id);
+  }
+
+  handleTodoDelete(id) {
+    this.props.actions.deleteTodo(id);
   }
 
   render(){
@@ -26,8 +40,20 @@ class Todo extends React.Component {
         <button onClick={this.handleSubmit.bind(this)}> Submit </button>
         <ul>
           {
+<<<<<<< HEAD:client/routes/TodoListView/containers/TodoList.js
             this.props.todoList.todos.map((todo) => {
               return <li>{todo.text}</li>
+=======
+            this.props.todolistReducer.todos.map((todo) => {
+            let currentID = todo.id;
+            return (
+                  <div>
+                    <li className={todo.completed ?classes.strikethrough:''}>{todo.text}</li>
+                    <button onClick={()=>this.handleTodoComplete(currentID)}>Complete</button>
+                    <button onClick={()=>this.handleTodoDelete(currentID)}>Delete</button>
+                  </div>
+              )
+>>>>>>> wolferian/master:client/containers/TodoList/Todo.js
             })
           }
         </ul>
@@ -40,5 +66,11 @@ function mapStateToProps(state) {
   return state;
 }
 
-export default connect(mapStateToProps)(Todo)
+function mapPropsToDispatch(dispatch) {
+  return {
+    actions: bindActionCreators(TodoActions, dispatch)
+  }
+}
+
+export default connect(mapStateToProps, mapPropsToDispatch)(Todo)
 
--- client/routes/TodoListView/modules/todolist.js
+++ client/routes/TodoListView/modules/todolist.js
@@ -1,25 +1,70 @@
+
 //Action
+let id = 1;
+
 export const addTodo = (text) => {
   return {
     type: 'ADD_TODO',
+    id: id++,
+    completed: false,
     text
   }
 }
 
+export const toggleTodo = (id) => {
+  return {
+    type: 'TOGGLE_TODO',
+    id
+  }
+}
+
+export const deleteTodo = (id) => {
+  return {
+    type: 'DELETE_TODO',
+    id
+  }
+}
+
+export const actions = {
+  addTodo,
+  toggleTodo,
+  deleteTodo
+}
+
 const initialState = {
-  todos:[{
-    text:'initial text'
+  todos: [{
+    text: 'initial text',
+    completed: false,
+    id:0
   }]
 }
 
 //Reducer
+<<<<<<< HEAD:client/routes/TodoListView/modules/todoList.js
 const todoListReducer = (state = initialState ,action) => {
   switch(action.type) {
+=======
+export default function todolistReducer(state = initialState, action) {
+  switch (action.type) {
+>>>>>>> wolferian/master:client/redux/modules/todolist.js
     case 'ADD_TODO':
       return Object.assign({}, state, {
         todos: [{
-          text: action.text
+          text: action.text,
+          id: action.id
         }, ...state.todos]
+      })
+    case 'TOGGLE_TODO':
+      return Object.assign({}, state, {
+        todos: state.todos.map((todo) => {
+          return todo.id === action.id ? Object.assign({}, todo, {completed: !todo.completed}) : todo;
+        })
+      })
+    case 'DELETE_TODO':
+      return Object.assign({},state, {
+        todos: state.todos.filter((todo)=>{
+          return todo.id !== action.id;
+        })
       })
     default:
       return state;
